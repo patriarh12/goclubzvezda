45265485dd7546d49cdf60a18d5fa7b6b9539541
Sergey Valyushitskiy <ny3blpu@gmail.com>
2018-02-01 16:22:47 +0300

    Table template for userlist was added

---
+++ src/components/UserList.vue
@@ -0,0 +1,55 @@
+<template>
+  <table v-else class="table table-hover">
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>Имя</th>
+      <th>Фамилия</th>
+      <th>Активен</th>
+      <th>Баланс</th>
+      <th>Email</th>
+      <th>Телефон</th>
+      <th>Зарегистрирован</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in users" v-bind:key="item.id">
+      <td># {{ item.id }}</td>
+      <td>{{ item.firstName }}</td>
+      <td>{{ item.lastName }}</td>
+      <td>{{ item.isActive }}</td>
+      <td>{{ item.balance }}</td>
+      <td>{{ item.email }}</td>
+      <td>{{ item.phone }}</td>
+      <td>{{ item.registered }}</td>
+    </tr>
+    </tbody>
+    <tfoot>
+    <tr>
+      <th colspan="8">
+        Всего пользователей: {{ total }}
+      </th>
+    </tr>
+    </tfoot>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: "UserListTable",
+    props: {
+      // Список пользователей
+      users: {
+        type: Array,
+        required: true
+      }
+    },
+    computed: {
+      // Общее количество пользователей
+      total: function() {
+        return this.users.length;
+      }
+    }
+  }
+</script>
+
--- src/components/UsersPage.vue
+++ src/components/UsersPage.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
-      USERS PAGE
+      <h2>Список пользователей</h2>
+
+      <div v-if="!list.length" class="alert alert-warning">
+        Загрузка...
+      </div>
+      <user-list v-else :users="list"></user-list>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "users-page"
+  import axios from 'axios'
+  import UserList from '@/components/UserList.vue'
+
+  export default {
+      name: "UsersPage",
+      components: {
+        UserList
+      },
+      data: () => ({
+        //Список пользователей
+        list: []
+      }),
+      methods: {
+        loadData() {
+          axios.get('http://localhost:3004/users/')
+            .then((response) => {
+              this.list = response.data;
+            })
+        }
+      },
+      mounted() {
+        this.loadData();
+      }
     }
 </script>
 
